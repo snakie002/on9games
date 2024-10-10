9fcd3d6ce00f1d896f3a68f2b67727a5aeba4387
snakie002 <snakie.ho@gmail.com>
2024-10-11 02:34:08 +1100

    Publish post xxx

--- src/_data/metadata.js
+++ src/_data/metadata.js
@@ -1,8 +1,8 @@
 module.exports = {
-	title: "on9games",
+	title: "on9games 享受遊戲",
 	url: "https://on9.games/",
 	language: "zh-Hant",
-	description: "on9games - POE 火炬之光無限 ARPG",
+	description: "on9games - POE 火炬之光無限 ARPG 遊戲攻略資訊",
 	author: {
 		name: "何師傅",
 		email: "on9.games.contact@gmail.com",
